324c116de77a1a57a983bd7f16f7a345bd30178d
J-nagendarreddy <jakka.17nagendarreddy@gmail.com>
2025-02-15 20:49:10 +0530

    Delete ampify-app directory

--- ampify-app/src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../styles/Footer.css";
-
-const Footer = () => {
-    return <footer className="footer">© 2025 Ampify - All Rights Reserved</footer>;
-};
-
-export default Footer;
