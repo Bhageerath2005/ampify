d92a50d11d11a9017652ec2f29254eef1d9d330a
Bhageerath2005 <kuntabhageerath55@gmail.com>
2025-02-15 21:50:46 +0530

    Delete ampify directory

--- ampify/src/Components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/Footer.css";
-
-const Footer = () => {
-    return <footer className="footer">© 2025 Ampify - All Rights Reserved</footer>;
-};
-
-export default Footer;
